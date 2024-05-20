b1117ba02c964478292a8d8053cae8330070bef9
xiara125 <86992014+xiara125@users.noreply.github.com>
2024-05-21 01:05:29 +0900

    plz

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,20 +1,44 @@
 const swconf = {
   
-    cacheName: 'chirpy-1716212687',resources: [
-      '/assets/css/jekyll-theme-chirpy.css',
-      '/',
+    cacheName: 'chirpy-1716221012',resources: [
+      '/xiara125/assets/css/jekyll-theme-chirpy.css',
+      '/xiara125/',
       
-        '/categories/',
+        '/xiara125/categories/',
       
-        '/tags/',
+        '/xiara125/tags/',
       
-        '/archives/',
+        '/xiara125/archives/',
       
-        '/about/',
+        '/xiara125/about/',
       
 
       
       
+        '/xiara125/_javascript/categories.js',
+        '/xiara125/_javascript/commons.js',
+        '/xiara125/_javascript/home.js',
+        '/xiara125/_javascript/misc.js',
+        '/xiara125/_javascript/modules/components/back-to-top.js',
+        '/xiara125/_javascript/modules/components/category-collapse.js',
+        '/xiara125/_javascript/modules/components/clipboard.js',
+        '/xiara125/_javascript/modules/components/img-loading.js',
+        '/xiara125/_javascript/modules/components/img-popup.js',
+        '/xiara125/_javascript/modules/components/locale-datetime.js',
+        '/xiara125/_javascript/modules/components/mode-watcher.js',
+        '/xiara125/_javascript/modules/components/search-display.js',
+        '/xiara125/_javascript/modules/components/sidebar.js',
+        '/xiara125/_javascript/modules/components/toc.js',
+        '/xiara125/_javascript/modules/components/tooltip-loader.js',
+        '/xiara125/_javascript/modules/layouts.js',
+        '/xiara125/_javascript/modules/layouts/basic.js',
+        '/xiara125/_javascript/modules/layouts/sidebar.js',
+        '/xiara125/_javascript/modules/layouts/topbar.js',
+        '/xiara125/_javascript/modules/plugins.js',
+        '/xiara125/_javascript/page.js',
+        '/xiara125/_javascript/post.js',
+        '/xiara125/_javascript/pwa/app.js',
+        '/xiara125/_javascript/pwa/sw.js'
     ],denyPaths: [
       
     ],
